a0dfb2495fa389d4c2bbc3a723f7725f377cd3ba
Vishnu C <vishnuc77@gmail.com>
2023-07-07 11:35:01 +0530

    ZKP-69 implements stro_to_felt with starknet-ff

--- pallets/support/src/lib.rs
+++ pallets/support/src/lib.rs
@@ -1,16 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+use starknet_ff::FieldElement;
 
 pub mod types;
 
 pub mod traits;
 
 pub fn str_to_felt(text: &str) -> u64 {
-    let b_text = text.as_bytes();
-    let mut result: u64 = 0;
-
-    for &byte in b_text {
-        result = (result << 8) + u64::from(byte);
-    }
-
-    result
+    let a = FieldElement::from_byte_slice_be(text.as_bytes());
+    u64::try_from(a.unwrap()).unwrap()
 }
