2fca21b0e78ca8b6f2af6a94a304a43e494b23a2
Vishnu C <vishnuc77@gmail.com>
2023-07-14 16:14:34 +0530

    ZKX-70 addresses PR comments

--- pallets/support/src/lib.rs
+++ pallets/support/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use crate::traits::FixedI128Ext;
 use sp_arithmetic::{fixed_point::FixedI128, traits::CheckedDiv, FixedPointNumber};
 use starknet_ff::FieldElement;
 
@@ -12,19 +13,21 @@ pub fn str_to_felt(text: &str) -> u64 {
 	u64::try_from(a.unwrap()).unwrap()
 }
 
-pub fn approximate(a: FixedI128, precision: u32) -> FixedI128 {
-	let inner_value: i128 = FixedI128::into_inner(a);
-	let divisor: i128 = 10_u64.pow(18).into();
-	let integer_part: i128 = inner_value / divisor;
-	let decimal_part: i128 = inner_value % divisor;
-	let ten_power_precision: i128 = 10_u64.pow(precision).into();
-	let decimal_part: FixedI128 = FixedI128::from_inner(decimal_part * ten_power_precision);
-	let frac_rounded: FixedI128 = decimal_part.round();
+impl FixedI128Ext<FixedI128> for FixedI128 {
+	fn round_to_precision(t: FixedI128, precision: u32) -> FixedI128 {
+		let inner_value: i128 = FixedI128::into_inner(t);
+		let divisor: i128 = 10_u64.pow(18).into();
+		let integer_part: i128 = inner_value / divisor;
+		let decimal_part: i128 = inner_value % divisor;
+		let ten_power_precision: i128 = 10_u64.pow(precision).into();
+		let decimal_part: FixedI128 = FixedI128::from_inner(decimal_part * ten_power_precision);
+		let frac_rounded: FixedI128 = decimal_part.round();
 
-	let ten_power_precision: FixedI128 =
-		FixedI128::checked_from_integer(ten_power_precision).unwrap();
-	let decimal_part: FixedI128 = frac_rounded.checked_div(&ten_power_precision).unwrap();
-	let integer_part: FixedI128 = FixedPointNumber::checked_from_integer(integer_part).unwrap();
-	let res: FixedI128 = FixedI128::add(integer_part, decimal_part);
-	res
+		let ten_power_precision: FixedI128 =
+			FixedI128::checked_from_integer(ten_power_precision).unwrap();
+		let decimal_part: FixedI128 = frac_rounded.checked_div(&ten_power_precision).unwrap();
+		let integer_part: FixedI128 = FixedPointNumber::checked_from_integer(integer_part).unwrap();
+		let res: FixedI128 = FixedI128::add(integer_part, decimal_part);
+		res
+	}
 }
--- pallets/support/src/traits.rs
+++ pallets/support/src/traits.rs
@@ -4,3 +4,7 @@ pub trait AssetInterface {
 	fn get_default_collateral() -> u64;
 	fn get_asset(id: u64) -> Option<Asset>;
 }
+
+pub trait FixedI128Ext<T> {
+	fn round_to_precision(t: T, precision: u32) -> T;
+}
