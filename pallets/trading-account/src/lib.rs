97856938fbd6ce4562b7758880c34c660fcf8f0b
Vishnu C <vishnuc77@gmail.com>
2023-07-18 15:25:22 +0530

    ZKP-71 implements basic trading account - WIP

--- pallets/trading-account/src/lib.rs
+++ pallets/trading-account/src/lib.rs
@@ -1,10 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
-
+use sp_arithmetic::fixed_point::FixedI128;
 
 #[cfg(test)]
 mod mock;
@@ -12,8 +9,6 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
 pub mod weights;
 pub use weights::*;
 
@@ -22,60 +17,43 @@ pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-	use zkx_support::types::TradingAccount;
 	use zkx_support::traits::AssetInterface;
+	use zkx_support::types::TradingAccount;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		/// Type representing the weight of this pallet
 		type WeightInfo: WeightInfo;
 		type Asset: AssetInterface;
 	}
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn accounts_count)]
+	pub(super) type AccountsCount<T: Config> = StorageValue<_, u128, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn accounts)]
-	pub type Accounts<T: Config> = StorageMap<
-	_,
-	Blake2_128Concat,
-		[u8;32],
-		TradingAccount,
-		OptionQuery>;
-
+	pub type Accounts<T: Config> = StorageMap<_, Blake2_128Concat, u8, TradingAccount, OptionQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn balances)]
 	pub(super) type Balances<T: Config> = StorageDoubleMap<
 		_,
-		Blake2_128Concat, [u8;32],
-		Blake2_128Concat, [u8;32],
-		sp_arithmetic::FixedI128,
-		ValueQuery
+		Blake2_128Concat,
+		[u8; 32],
+		Blake2_128Concat,
+		u8,
+		FixedI128,
+		ValueQuery,
 	>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
-		AccountAdded { account_id: [u8; 32]},
-		DefaultCollateral { id: u64 },
+		AccountAdded { account_id: [u8; 32] },
 	}
 
 	// Errors inform users that something went wrong.
@@ -92,70 +70,20 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::call_index(0)]
-		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::call_index(1)]
-		#[pallet::weight(T::WeightInfo::cause_error())]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
-
 		#[pallet::call_index(2)]
 		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn record_account(origin: OriginFor<T>, trading_account: TradingAccount,) -> DispatchResult {
+		pub fn record_account(
+			origin: OriginFor<T>,
+			trading_account: TradingAccount,
+		) -> DispatchResult {
 			let _who = ensure_signed(origin)?;
 
-			<Accounts<T>>::insert(trading_account.account_id, trading_account.clone());
+			// <Accounts<T>>::insert(trading_account.account_id, trading_account.clone());
 			let default_collateral = T::Asset::get_default_collateral();
-
-			Self::deposit_event(Event::DefaultCollateral { id: default_collateral });
 
 			Self::deposit_event(Event::AccountAdded { account_id: trading_account.account_id });
 
 			Ok(())
-			// Read a value from storage.
-/*			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}*/
 		}
 	}
 }
