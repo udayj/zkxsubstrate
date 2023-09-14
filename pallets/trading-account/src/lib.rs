749b422987292db004842dd8d44598959e9b10e5
Vishnu C <vishnuc77@gmail.com>
2023-09-14 23:06:17 +0530

    ZKP-290 implements user balance transfer events

--- pallets/trading-account/src/lib.rs
+++ pallets/trading-account/src/lib.rs
@@ -424,6 +424,11 @@ pub mod pallet {
 			AccountPresenceMap::<T>::contains_key(&account)
 		}
 
+		fn get_account(account_id: &U256) -> Option<TradingAccount> {
+			let trading_account = AccountMap::<T>::get(&account_id)?;
+			Some(trading_account)
+		}
+
 		fn get_public_key(account: &U256) -> Option<U256> {
 			let trading_account = AccountMap::<T>::get(&account)?;
 			Some(trading_account.pub_key)
