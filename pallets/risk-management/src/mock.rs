108b1fe818e468ce2cdda45cf6b24a471e1bbd33
Swathi Reddy <swathireddyg54@gmail.com>
2023-09-25 10:59:44 +0530

    ZKP-325 adds tests for risk management

---
+++ pallets/risk-management/src/mock.rs
@@ -0,0 +1,118 @@
+use crate as pallet_risk_management;
+use frame_support::traits::{ConstU16, ConstU64};
+use pallet_asset;
+use pallet_market;
+use pallet_market_prices;
+use pallet_timestamp;
+use pallet_trading;
+use pallet_trading_fees;
+use pallet_zkx_trading_account;
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+};
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system,
+		Markets: pallet_market,
+		MarketPrices: pallet_market_prices,
+		Assets: pallet_asset,
+		RiskManagement: pallet_risk_management,
+		Timestamp: pallet_timestamp,
+		TradingAccounts: pallet_zkx_trading_account,
+		TradingFees: pallet_trading_fees,
+		Trading: pallet_trading,
+	}
+);
+
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ConstU16<42>;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+impl pallet_asset::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+}
+
+impl pallet_market::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type AssetPallet = Assets;
+}
+
+impl pallet_market_prices::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type MarketPallet = Markets;
+	type TimeProvider = Timestamp;
+}
+
+impl pallet_trading_fees::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+}
+
+impl pallet_zkx_trading_account::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type AssetPallet = Assets;
+	type MarketPallet = Markets;
+	type MarketPricesPallet = MarketPrices;
+	type TradingPallet = Trading;
+}
+
+impl pallet_risk_management::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type MarketPallet = Markets;
+	type TradingPallet = Trading;
+	type TradingAccountPallet = TradingAccounts;
+}
+
+impl pallet_trading::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type MarketPallet = Markets;
+	type MarketPricesPallet = MarketPrices;
+	type RiskManagementPallet = RiskManagement;
+	type TradingAccountPallet = TradingAccounts;
+	type TradingFeesPallet = TradingFees;
+}
+
+impl pallet_timestamp::Config for Test {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = ConstU64<5>;
+	type WeightInfo = ();
+}
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
---
+++ pallets/risk-management/src/tests.rs
@@ -0,0 +1,244 @@
+use crate::{mock::*, Event};
+use frame_support::assert_ok;
+use primitive_types::U256;
+use sp_io::hashing::blake2_256;
+use starknet_crypto::{sign, verify, FieldElement};
+use zkx_support::traits::{FieldElementExt, Hashable, U256Ext};
+use zkx_support::types::{
+	Asset, BalanceUpdate, Direction, HashType, Market, Order, OrderType, Position, Side,
+	TimeInForce, TradingAccountWithoutId,
+};
+
+fn setup() -> (Vec<Market>, Vec<TradingAccountWithoutId>, Vec<U256>) {
+	let ETH_ID: U256 = 4543560.into();
+	let USDC_ID: U256 = 1431520323.into();
+	let LINK_ID: U256 = 1279872587.into();
+	let BTC_ID: U256 = 4346947.into();
+	let name1: Vec<u8> = "ETH".into();
+	let asset1: Asset = Asset {
+		id: ETH_ID,
+		name: name1.try_into().unwrap(),
+		is_tradable: true,
+		is_collateral: false,
+		token_decimal: 18,
+	};
+	let name2: Vec<u8> = "USDC".into();
+	let asset2: Asset = Asset {
+		id: USDC_ID,
+		name: name2.try_into().unwrap(),
+		is_tradable: false,
+		is_collateral: true,
+		token_decimal: 6,
+	};
+	let name3: Vec<u8> = "LINK".into();
+	let asset3: Asset = Asset {
+		id: LINK_ID,
+		name: name3.try_into().unwrap(),
+		is_tradable: true,
+		is_collateral: false,
+		token_decimal: 6,
+	};
+
+	let assets: Vec<Asset> = vec![asset1.clone(), asset2.clone(), asset3.clone()];
+	assert_ok!(Assets::replace_all_assets(RuntimeOrigin::signed(1), assets));
+
+	let market1: Market = Market {
+		id: 1.into(),
+		asset: ETH_ID,
+		asset_collateral: USDC_ID,
+		is_tradable: true,
+		is_archived: false,
+		ttl: 3600,
+		tick_size: 1.into(),
+		tick_precision: 1,
+		step_size: 1.into(),
+		step_precision: 1,
+		minimum_order_size: 1.into(),
+		minimum_leverage: 1.into(),
+		maximum_leverage: 10.into(),
+		currently_allowed_leverage: 8.into(),
+		maintenance_margin_fraction: 1.into(),
+		initial_margin_fraction: 1.into(),
+		incremental_initial_margin_fraction: 1.into(),
+		incremental_position_size: 1.into(),
+		baseline_position_size: 1.into(),
+		maximum_position_size: 1.into(),
+	};
+	let market2: Market = Market {
+		id: 2.into(),
+		asset: LINK_ID,
+		asset_collateral: USDC_ID,
+		is_tradable: false,
+		is_archived: false,
+		ttl: 360,
+		tick_size: 1.into(),
+		tick_precision: 1,
+		step_size: 1.into(),
+		step_precision: 1,
+		minimum_order_size: 1.into(),
+		minimum_leverage: 1.into(),
+		maximum_leverage: 10.into(),
+		currently_allowed_leverage: 8.into(),
+		maintenance_margin_fraction: 1.into(),
+		initial_margin_fraction: 1.into(),
+		incremental_initial_margin_fraction: 1.into(),
+		incremental_position_size: 1.into(),
+		baseline_position_size: 1.into(),
+		maximum_position_size: 1.into(),
+	};
+
+	let markets: Vec<Market> = vec![market1.clone(), market2.clone()];
+	assert_ok!(Markets::replace_all_markets(RuntimeOrigin::signed(1), markets.clone()));
+
+	let user_pub_key_1: U256 = U256::from_dec_str(
+		"454932787469224290468444410084879070088819078827906347654495047407276534283",
+	)
+	.unwrap();
+	let user_pri_key_1: U256 = U256::from_dec_str(
+		"217039137810971208563823259722717297948702641410765313684702872265493782699",
+	)
+	.unwrap();
+	let user_address_1: U256 = U256::from(100_u8);
+
+	let user_pub_key_2: U256 = U256::from_dec_str(
+		"2101677845476848141002376837472833021659088026888369432434421980160153750090",
+	)
+	.unwrap();
+	let user_pri_key_2: U256 = U256::from_dec_str(
+		"2835524789612495000294332407161775540542356260492319813526822636942276039073",
+	)
+	.unwrap();
+	let user_address_2: U256 = U256::from(101_u8);
+
+	let user_1 = TradingAccountWithoutId {
+		account_address: user_address_1,
+		index: 0,
+		pub_key: user_pub_key_1,
+	};
+	let user_2 = TradingAccountWithoutId {
+		account_address: user_address_2,
+		index: 0,
+		pub_key: user_pub_key_2,
+	};
+	let accounts: Vec<TradingAccountWithoutId> = vec![user_1, user_2];
+	assert_ok!(TradingAccounts::add_accounts(RuntimeOrigin::signed(1), accounts.clone()));
+
+	let private_keys: Vec<U256> = vec![user_pri_key_1, user_pri_key_2];
+
+	(markets, accounts, private_keys)
+}
+
+fn get_trading_account_id(trading_accounts: Vec<TradingAccountWithoutId>, index: usize) -> U256 {
+	let account_address = U256::from(trading_accounts[index].account_address);
+	let mut account_array: [u8; 32] = [0; 32];
+	account_address.to_little_endian(&mut account_array);
+
+	let mut concatenated_bytes: Vec<u8> = account_array.to_vec();
+	concatenated_bytes.push(trading_accounts.get(index).unwrap().index);
+	let result: [u8; 33] = concatenated_bytes.try_into().unwrap();
+
+	let trading_account_id: U256 = blake2_256(&result).into();
+	trading_account_id
+}
+
+fn sign_order(order: Order, private_key: U256) -> Order {
+	let order_hash = order.hash(&order.hash_type).unwrap();
+	let private_key = private_key.try_to_felt().unwrap();
+	let signature = sign(&private_key, &order_hash, &FieldElement::ONE).unwrap();
+
+	let sig_r = signature.r.to_u256();
+	let sig_s = signature.s.to_u256();
+	Order { sig_r, sig_s, ..order }
+}
+
+#[test]
+// basic open trade without any leverage
+fn test_should_calculate_correct_liq_usdc_collateral_1() {
+	new_test_ext().execute_with(|| {
+		let order_id_1 = 200_u128;
+		let order_id_2 = 201_u128;
+
+		let (markets, accounts, private_keys) = setup();
+		// Go past genesis block so events get deposited
+		System::set_block_number(1);
+
+		let account_id_1: U256 = get_trading_account_id(accounts.clone(), 0);
+		let account_id_2: U256 = get_trading_account_id(accounts.clone(), 1);
+
+		let order_1 = Order {
+			account_id: account_id_1,
+			order_id: order_id_1,
+			market_id: markets[0].id,
+			order_type: OrderType::Limit,
+			direction: Direction::Long,
+			side: Side::Buy,
+			price: 100.into(),
+			size: 1.into(),
+			leverage: 1.into(),
+			slippage: 10.into(),
+			post_only: false,
+			time_in_force: TimeInForce::GTC,
+			sig_r: 0.into(),
+			sig_s: 0.into(),
+			hash_type: HashType::Pedersen,
+		};
+		let order_2 = Order {
+			account_id: account_id_2,
+			order_id: order_id_2,
+			market_id: markets[0].id,
+			order_type: OrderType::Market,
+			direction: Direction::Short,
+			side: Side::Buy,
+			price: 100.into(),
+			size: 1.into(),
+			leverage: 1.into(),
+			slippage: 10.into(),
+			post_only: false,
+			time_in_force: TimeInForce::GTC,
+			sig_r: 0.into(),
+			sig_s: 0.into(),
+			hash_type: HashType::Pedersen,
+		};
+
+		let order_1 = sign_order(order_1, private_keys[0]);
+		let order_2 = sign_order(order_2, private_keys[1]);
+		let orders: Vec<Order> = vec![order_1, order_2];
+
+		assert_ok!(Trading::execute_trade(
+			RuntimeOrigin::signed(1),
+			U256::from(1_u8),
+			1.into(),
+			markets[0].id,
+			100.into(),
+			orders
+		));
+
+		// System::assert_has_event(Event::OrderError { order_id: 12, error_code: 25 }.into());
+
+		let position1 = Trading::positions(account_id_1, [markets[0].id, U256::from(1_u8)]);
+		let expected_position: Position = Position {
+			avg_execution_price: 100.into(),
+			size: 1.into(),
+			direction: Direction::Long,
+			side: Side::Buy,
+			margin_amount: 100.into(),
+			borrowed_amount: 0.into(),
+			leverage: 1.into(),
+			realized_pnl: 0.into(),
+		};
+		assert_eq!(expected_position, position1);
+
+		let position2 = Trading::positions(account_id_2, [markets[0].id, U256::from(2_u8)]);
+		let expected_position: Position = Position {
+			avg_execution_price: 100.into(),
+			size: 1.into(),
+			direction: Direction::Short,
+			side: Side::Buy,
+			margin_amount: 100.into(),
+			borrowed_amount: 0.into(),
+			leverage: 1.into(),
+			realized_pnl: 0.into(),
+		};
+		assert_eq!(expected_position, position2);
+	});
+}
