0c647f138202c61449a4d20298b7f98c1ef8af3d
Sachin Jangid <sachin@zkx.fi>
2023-10-03 17:52:48 +0530

    ZKP-380: config updated

--- test-scripts/config.ts
+++ test-scripts/config.ts
@@ -3,11 +3,11 @@ import * as dotenv from 'dotenv';
 dotenv.config(); 
 
 const {
-    wsUrl = 'ws://127.0.0.1:9944',
-    nodeAccount = '//Alice',
+    WS_URL = 'ws://127.0.0.1:9944',
+    NODE_ACCOUNT = '//Alice',
 } = process.env;
 
 export const config = {
-    wsUrl,
-    nodeAccount,
+    wsUrl: WS_URL,
+    nodeAccount: NODE_ACCOUNT,
 }
