61bc6754effbfca2c92a73b59090c1622f9d104f
merkle-groot <visheh10@gmail.com>
2023-10-30 22:15:06 +0530

    ZKP-502 Working Trading test with refactoring

--- pallets/trading/src/tests.rs
+++ pallets/trading/src/tests.rs
@@ -5,8 +5,13 @@ use sp_arithmetic::FixedI128;
 use sp_io::hashing::blake2_256;
 use sp_runtime::BoundedVec;
 use starknet_crypto::{sign, FieldElement};
-use zkx_support::test_helpers::accounts_helper::get_trading_account_id;
+use zkx_support::test_helpers::accounts_helper::{
+	alice, bob, charlie, create_withdrawal_request, dave, eduard, get_private_key,
+	get_trading_account_id,
+};
 use zkx_support::test_helpers::asset_helper::{btc, eth, link, usdc};
+use zkx_support::test_helpers::market_helper::{btc_usdc, link_usdc};
+use zkx_support::test_helpers::trading_helper::setup_fee;
 use zkx_support::traits::{FieldElementExt, Hashable, U256Ext};
 use zkx_support::types::{
 	BaseFee, Direction, Discount, ExtendedMarket, HashType, Market, Order, OrderType, Position,
@@ -20,265 +25,79 @@ const ORDER_ID_4: u128 = 203_u128;
 const ORDER_ID_5: u128 = 204_u128;
 const ORDER_ID_6: u128 = 205_u128;
 
-fn setup() -> (Vec<ExtendedMarket>, Vec<TradingAccountMinimal>, Vec<U256>) {
-	assert_ok!(Timestamp::set(None.into(), 100));
+fn setup() -> sp_io::TestExternalities {
+	// Create a new test environment
+	let mut env = new_test_ext();
 
-	let assets = vec![eth(), usdc(), link(), btc()];
-	assert_ok!(Assets::replace_all_assets(RuntimeOrigin::signed(1), assets));
+	// Set the block number in the environment
+	env.execute_with(|| {
+		// Set the block number
+		System::set_block_number(1);
+		assert_ok!(Timestamp::set(None.into(), 100));
 
-	let market1 = ExtendedMarket {
-		market: Market {
-			id: 1,
-			version: 1,
-			asset: btc().asset.id,
-			asset_collateral: usdc().asset.id,
-			is_tradable: true,
-			is_archived: false,
-			ttl: 3600,
-			tick_size: 1.into(),
-			tick_precision: 1,
-			step_size: 1.into(),
-			step_precision: 1,
-			minimum_order_size: 1.into(),
-			minimum_leverage: 1.into(),
-			maximum_leverage: 10.into(),
-			currently_allowed_leverage: 8.into(),
-			maintenance_margin_fraction: 1.into(),
-			initial_margin_fraction: 1.into(),
-			incremental_initial_margin_fraction: 1.into(),
-			incremental_position_size: 1.into(),
-			baseline_position_size: 1.into(),
-			maximum_position_size: 1.into(),
-		},
-		metadata_url: BoundedVec::new(),
-	};
+		// Set the assets in the system
+		assert_ok!(Assets::replace_all_assets(
+			RuntimeOrigin::signed(1),
+			vec![eth(), usdc(), link(), btc()]
+		));
+		assert_ok!(Markets::replace_all_markets(
+			RuntimeOrigin::signed(1),
+			vec![btc_usdc(), link_usdc()]
+		));
 
-	let market2 = ExtendedMarket {
-		market: Market {
-			id: 2,
-			version: 1,
-			asset: link().asset.id,
-			asset_collateral: usdc().asset.id,
-			is_tradable: false,
-			is_archived: false,
-			ttl: 360,
-			tick_size: 1.into(),
-			tick_precision: 1,
-			step_size: 1.into(),
-			step_precision: 1,
-			minimum_order_size: 1.into(),
-			minimum_leverage: 1.into(),
-			maximum_leverage: 10.into(),
-			currently_allowed_leverage: 8.into(),
-			maintenance_margin_fraction: 1.into(),
-			initial_margin_fraction: 1.into(),
-			incremental_initial_margin_fraction: 1.into(),
-			incremental_position_size: 1.into(),
-			baseline_position_size: 1.into(),
-			maximum_position_size: 1.into(),
-		},
-		metadata_url: BoundedVec::new(),
-	};
+		// Add accounts to the system
+		assert_ok!(TradingAccounts::add_accounts(
+			RuntimeOrigin::signed(1),
+			vec![alice(), bob(), charlie(), dave()]
+		));
+	});
 
-	let markets = vec![market1.clone(), market2.clone()];
-	assert_ok!(Markets::replace_all_markets(RuntimeOrigin::signed(1), markets.clone()));
-
-	let user_pub_key_1: U256 = U256::from_dec_str(
-		"454932787469224290468444410084879070088819078827906347654495047407276534283",
-	)
-	.unwrap();
-	let user_pri_key_1: U256 = U256::from_dec_str(
-		"217039137810971208563823259722717297948702641410765313684702872265493782699",
-	)
-	.unwrap();
-	let user_address_1: U256 = U256::from(100_u8);
-
-	let user_pub_key_2: U256 = U256::from_dec_str(
-		"2101677845476848141002376837472833021659088026888369432434421980160153750090",
-	)
-	.unwrap();
-	let user_pri_key_2: U256 = U256::from_dec_str(
-		"2835524789612495000294332407161775540542356260492319813526822636942276039073",
-	)
-	.unwrap();
-	let user_address_2: U256 = U256::from(101_u8);
-
-	let user_pub_key_3: U256 = U256::from_dec_str(
-		"1927799101328918885926814969993421873905724180750168745093131010179897850144",
-	)
-	.unwrap();
-	let user_pri_key_3: U256 = U256::from_dec_str(
-		"3388506857955987752046415916181604993164423072000548640801744803879383940670",
-	)
-	.unwrap();
-	let user_address_3: U256 = U256::from(102_u8);
-
-	let user_pub_key_4: U256 = U256::from_dec_str(
-		"824120678599933675767871867465569325984720238047137957464936400424120564339",
-	)
-	.unwrap();
-	let user_pri_key_4: U256 = U256::from_dec_str(
-		"84035867551811388210596922086133550045728262314839423570645036080104955628",
-	)
-	.unwrap();
-	let user_address_4: U256 = U256::from(103_u8);
-
-	let user_1 = TradingAccountMinimal {
-		account_address: user_address_1,
-		index: 0,
-		pub_key: user_pub_key_1,
-	};
-	let user_2 = TradingAccountMinimal {
-		account_address: user_address_2,
-		index: 0,
-		pub_key: user_pub_key_2,
-	};
-	let user_3 = TradingAccountMinimal {
-		account_address: user_address_3,
-		index: 0,
-		pub_key: user_pub_key_3,
-	};
-	let user_4 = TradingAccountMinimal {
-		account_address: user_address_4,
-		index: 0,
-		pub_key: user_pub_key_4,
-	};
-	let accounts: Vec<TradingAccountMinimal> = vec![user_1, user_2, user_3, user_4];
-	assert_ok!(TradingAccounts::add_accounts(RuntimeOrigin::signed(1), accounts.clone()));
-
-	let private_keys: Vec<U256> =
-		vec![user_pri_key_1, user_pri_key_2, user_pri_key_3, user_pri_key_4];
-
-	(markets, accounts, private_keys)
-}
-
-fn setup_fee() -> (Vec<u8>, Vec<BaseFee>, Vec<u8>, Vec<Discount>) {
-	let fee_tiers: Vec<u8> = vec![1, 2, 3];
-	let mut fee_details: Vec<BaseFee> = Vec::new();
-	let base_fee1 = BaseFee {
-		number_of_tokens: 0.into(),
-		maker_fee: FixedI128::from_inner(20000000000000000),
-		taker_fee: FixedI128::from_inner(50000000000000000),
-	};
-	let base_fee2 = BaseFee {
-		number_of_tokens: 1000.into(),
-		maker_fee: FixedI128::from_inner(15000000000000000),
-		taker_fee: FixedI128::from_inner(40000000000000000),
-	};
-	let base_fee3 = BaseFee {
-		number_of_tokens: 5000.into(),
-		maker_fee: FixedI128::from_inner(10000000000000000),
-		taker_fee: FixedI128::from_inner(35000000000000000),
-	};
-	fee_details.push(base_fee1);
-	fee_details.push(base_fee2);
-	fee_details.push(base_fee3);
-
-	let discount_tiers: Vec<u8> = vec![1, 2, 3, 4];
-	let mut discount_details: Vec<Discount> = Vec::new();
-	let discount1 =
-		Discount { number_of_tokens: 0.into(), discount: FixedI128::from_inner(30000000000000000) };
-	let discount2 = Discount {
-		number_of_tokens: 1000.into(),
-		discount: FixedI128::from_inner(50000000000000000),
-	};
-	let discount3 = Discount {
-		number_of_tokens: 4000.into(),
-		discount: FixedI128::from_inner(75000000000000000),
-	};
-	let discount4 = Discount {
-		number_of_tokens: 7500.into(),
-		discount: FixedI128::from_inner(100000000000000000),
-	};
-	discount_details.push(discount1);
-	discount_details.push(discount2);
-	discount_details.push(discount3);
-	discount_details.push(discount4);
-
-	(fee_tiers, fee_details, discount_tiers, discount_details)
-}
-
-fn sign_order(order: Order, private_key: U256) -> Order {
-	let order_hash = order.hash(&order.signature_info.hash_type).unwrap();
-	let private_key = private_key.try_to_felt().unwrap();
-	let signature = sign(&private_key, &order_hash, &FieldElement::ONE).unwrap();
-
-	let sig_r = signature.r.to_u256();
-	let sig_s = signature.s.to_u256();
-	let signature_info = SignatureInfo { sig_r, sig_s, ..order.signature_info };
-	Order { signature_info, ..order }
+	env
 }
 
 #[test]
 // basic open trade without any leverage
 fn it_works_for_open_trade_simple() {
-	new_test_ext().execute_with(|| {
-		let (markets, accounts, private_keys) = setup();
+	let mut env = setup();
+
+	env.execute_with(|| {
 		// Go past genesis block so events get deposited
 		System::set_block_number(1);
 
-		let account_id_1: U256 = get_trading_account_id(accounts[0]);
-		let account_id_2: U256 = get_trading_account_id(accounts[1]);
+		// Generate account_ids
+		let alice_id: U256 = get_trading_account_id(alice());
+		let bob_id: U256 = get_trading_account_id(bob());
 
-		let order_1 = Order {
-			account_id: account_id_1,
-			order_id: ORDER_ID_1,
-			market_id: markets[0].market.id,
-			order_type: OrderType::Limit,
-			direction: Direction::Long,
-			side: Side::Buy,
-			price: 100.into(),
-			size: 1.into(),
-			leverage: 1.into(),
-			slippage: FixedI128::from_inner(100000000000000000),
-			post_only: false,
-			time_in_force: TimeInForce::GTC,
-			signature_info: SignatureInfo {
-				liquidator_pub_key: U256::zero(),
-				hash_type: HashType::Pedersen,
-				sig_r: U256::zero(),
-				sig_s: U256::zero(),
-			},
-		};
-		let order_2 = Order {
-			account_id: account_id_2,
-			order_id: ORDER_ID_2,
-			market_id: markets[0].market.id,
-			order_type: OrderType::Market,
-			direction: Direction::Short,
-			side: Side::Buy,
-			price: 100.into(),
-			size: 1.into(),
-			leverage: 1.into(),
-			slippage: FixedI128::from_inner(100000000000000000),
-			post_only: false,
-			time_in_force: TimeInForce::GTC,
-			signature_info: SignatureInfo {
-				liquidator_pub_key: U256::zero(),
-				hash_type: HashType::Pedersen,
-				sig_r: U256::zero(),
-				sig_s: U256::zero(),
-			},
-		};
+		// market id
+		let market_id = btc_usdc().market.id;
 
-		let order_1 = sign_order(order_1, private_keys[0]);
-		let order_2 = sign_order(order_2, private_keys[1]);
-		let orders: Vec<Order> = vec![order_1, order_2];
+		// Create orders
+		let alice_order = Order::new().sign_order(get_private_key(alice().pub_key));
+		let bob_order = Order::new()
+			.set_account_id(bob_id)
+			.set_order_id(201_u128)
+			.set_direction(Direction::Short)
+			.set_order_type(OrderType::Market)
+			.sign_order(get_private_key(bob().pub_key));
 
 		assert_ok!(Trading::execute_trade(
 			RuntimeOrigin::signed(1),
+			// batch_id
 			U256::from(1_u8),
+			// quantity_locked
 			1.into(),
-			markets[0].market.id,
+			// market_id
+			market_id,
+			// oracle_price
 			100.into(),
-			orders
+			// orders
+			vec![alice_order, bob_order]
 		));
 
-		// System::assert_has_event(Event::OrderError { order_id: 12, error_code: 25 }.into());
-
-		let position1 = Trading::positions(account_id_1, (markets[0].market.id, Direction::Long));
+		// Check the execution of orders
+		let alice_position = Trading::positions(alice_id, (market_id, Direction::Long));
 		let expected_position: Position = Position {
-			market_id: markets[0].market.id,
+			market_id,
 			avg_execution_price: 100.into(),
 			size: 1.into(),
 			direction: Direction::Long,
@@ -288,11 +107,11 @@ fn it_works_for_open_trade_simple() {
 			leverage: 1.into(),
 			realized_pnl: 0.into(),
 		};
-		assert_eq!(expected_position, position1);
+		assert_eq!(expected_position, alice_position);
 
-		let position2 = Trading::positions(account_id_2, (markets[0].market.id, Direction::Short));
+		let bob_position = Trading::positions(bob_id, (market_id, Direction::Short));
 		let expected_position: Position = Position {
-			market_id: markets[0].market.id,
+			market_id,
 			avg_execution_price: 100.into(),
 			size: 1.into(),
 			direction: Direction::Short,
@@ -302,7 +121,7 @@ fn it_works_for_open_trade_simple() {
 			leverage: 1.into(),
 			realized_pnl: 0.into(),
 		};
-		assert_eq!(expected_position, position2);
+		assert_eq!(expected_position, bob_position);
 	});
 }
 
