0589ff3e4675a0118c27b538a726e4cba8f518bf
merkle-groot <visheh10@gmail.com>
2023-11-16 10:52:05 +0530

    ZKP-567 Added imports

--- pallets/abr/src/lib.rs
+++ pallets/abr/src/lib.rs
@@ -4,9 +4,10 @@ pub use pallet::*;
 
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
-	// use frame_support::pallet_prelude::*;
-	// use frame_system::pallet_prelude::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
 	use pallet_support::traits::TradingAccountInterface;
+	use sp_arithmetic::{fixed_point::FixedI128, FixedI128};
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
@@ -14,5 +15,16 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type TradingAccountPallet: TradingAccountInterface;
+	}
+
+	impl<T: Config> Pallet<T> {
+
+		fn calculate_sliding_mean(prices: Vec<FixedI128>) -> Vec<FixedI128> {
+			let sliding_mean = Vec<FixedI128>;
+		}
+
+		fn calculate_abr(mark_prices: Vec<FixedI128>, index_prices: Vec<FixedI128>) -> () {
+			
+		}
 	}
 }
