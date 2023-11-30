00ad2b66a926e9c8f411697693bbe8e32b8269b2
Swathi Reddy <swathireddyg54@gmail.com>
2023-11-30 11:07:30 +0530

    ZKP-596 debugging abr tests

--- pallets/prices/src/tests.rs
+++ pallets/prices/src/tests.rs
@@ -2,15 +2,18 @@ use crate::mock::*;
 use frame_support::assert_ok;
 use pallet_support::{
 	test_helpers::{
-		asset_helper::{eth, link, usdc},
-		market_helper::{eth_usdc, link_usdc},
+		accounts_helper::{alice, bob, charlie, dave, get_private_key, get_trading_account_id},
+		asset_helper::{btc, eth, link, usdc},
+		market_helper::{btc_usdc, eth_usdc, link_usdc},
 	},
-	types::MultiplePrices,
+	types::{Direction, MultiplePrices, Order, OrderType, Position, Side},
 };
+use primitive_types::U256;
 use sp_arithmetic::fixed_point::FixedI128;
 
 // declare test_helper module
 pub mod test_helper;
+use sp_io::transaction_index::index;
 use test_helper::*;
 
 fn setup() -> sp_io::TestExternalities {
@@ -29,6 +32,66 @@ fn setup() -> sp_io::TestExternalities {
 	});
 
 	test_env.into()
+}
+
+fn setup_trading() -> sp_io::TestExternalities {
+	// Create a new test environment
+	let mut env = new_test_ext();
+
+	// Set the block number in the environment
+	env.execute_with(|| {
+		// Set the block number
+		System::set_block_number(1);
+		assert_ok!(Timestamp::set(None.into(), 1699940367000));
+
+		// Set the assets in the system
+		assert_ok!(AssetModule::replace_all_assets(
+			RuntimeOrigin::signed(1),
+			vec![eth(), usdc(), link(), btc()]
+		));
+		assert_ok!(MarketModule::replace_all_markets(
+			RuntimeOrigin::signed(1),
+			vec![btc_usdc(), link_usdc()]
+		));
+
+		// Add accounts to the system
+		assert_ok!(TradingAccounts::add_accounts(
+			RuntimeOrigin::signed(1),
+			vec![alice(), bob(), charlie(), dave()]
+		));
+
+		// Set ABR interval as 8 hours
+		assert_ok!(PricesModule::set_abr_interval(RuntimeOrigin::signed(1), 28800));
+
+		// Set Base ABR as 0.000025
+		assert_ok!(PricesModule::set_base_abr(
+			RuntimeOrigin::signed(1),
+			FixedI128::from_inner(25000000000000)
+		));
+
+		// Set Bollinger width as 1.5
+		assert_ok!(PricesModule::set_bollinger_width(
+			RuntimeOrigin::signed(1),
+			FixedI128::from_inner(1500000000000000000)
+		));
+
+		// Set no.of users per batch
+		assert_ok!(PricesModule::set_no_of_users_per_batch(RuntimeOrigin::signed(1), 10));
+	});
+	env
+}
+
+fn set_prices(market_id: u128) {
+	let (mark_prices, index_prices) = mock_prices::get_btc_usdc_prices_1();
+	let mut interval: u64 = 0;
+	for i in 0..mark_prices.len() {
+		let mut prices: Vec<MultiplePrices> = Vec::new();
+		let price: MultiplePrices =
+			MultiplePrices { market_id, index_price: index_prices[i], mark_price: mark_prices[i] };
+		prices.push(price);
+		assert_ok!(PricesModule::update_prices(RuntimeOrigin::signed(1), prices, interval));
+		interval += 60;
+	}
 }
 
 #[test]
@@ -348,5 +411,76 @@ fn test_abr_different_length() {
 
 			compare_with_threshold(result.0, expected_results[iterator], convert_to_fixed(1e-10));
 		}
+	});
+}
+
+#[test]
+fn test_abr_flow_for_btc_orders() {
+	let mut env = setup_trading();
+
+	env.execute_with(|| {
+		// Generate account_ids
+		let alice_id: U256 = get_trading_account_id(alice());
+		let bob_id: U256 = get_trading_account_id(bob());
+
+		// market id
+		let market_id = btc_usdc().market.id;
+
+		// Create orders
+		let alice_order =
+			Order::new(201_u128, alice_id).sign_order(get_private_key(alice().pub_key));
+		let bob_order = Order::new(202_u128, bob_id)
+			.set_direction(Direction::Short)
+			.set_order_type(OrderType::Market)
+			.sign_order(get_private_key(bob().pub_key));
+
+		assert_ok!(Trading::execute_trade(
+			RuntimeOrigin::signed(1),
+			// batch_id
+			U256::from(1_u8),
+			// quantity_locked
+			1.into(),
+			// market_id
+			market_id,
+			// oracle_price
+			100.into(),
+			// orders
+			vec![alice_order.clone(), bob_order.clone()],
+			// batch_timestamp
+			1699940278000
+		));
+
+		// Change block timestamp
+		Timestamp::set_timestamp(1699940306800);
+
+		// Set ABR timestamp
+		assert_ok!(PricesModule::set_abr_timestamp(RuntimeOrigin::signed(1), 1699940335600));
+
+		let abr_state = PricesModule::abr_state();
+		println!("abr_state {:?}", abr_state);
+
+		let epoch_to_timestamp = PricesModule::epoch_to_timestamp(1);
+		println!("epoch_to_timestamp {:?}", epoch_to_timestamp);
+
+		// Set mark and index prices
+		set_prices(market_id);
+
+		// Compute ABR value
+		assert_ok!(PricesModule::set_abr_value(RuntimeOrigin::signed(1), market_id));
+
+		let epoch_to_abr_value = PricesModule::epoch_market_to_abr_value(1, market_id);
+		println!("epoch_to_abr_value: {:?}", epoch_to_abr_value);
+
+		let epoch_market_to_last_price = PricesModule::epoch_market_to_last_price(1, market_id);
+		println!("epoch_market_to_last_price: {:?}", epoch_market_to_last_price);
+
+		let balance = TradingAccounts::balances(alice_id, btc_usdc().market.asset_collateral);
+		println!("Alice balance: {:?}", balance);
+
+		let balance = TradingAccounts::balances(bob_id, btc_usdc().market.asset_collateral);
+		println!("Bob balance: {:?}", balance);
+
+		// Pay ABR
+		assert_ok!(PricesModule::make_abr_payments(RuntimeOrigin::signed(1)));
 	});
 }
