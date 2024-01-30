39f6faa2c0740baa3e8c0c83c066022640517880
Swathi Reddy <swathireddyg54@gmail.com>
2024-01-30 08:53:16 +0530

    Fixes user balnce being negative

--- pallets/prices/src/lib.rs
+++ pallets/prices/src/lib.rs
@@ -1013,7 +1013,7 @@ pub mod pallet {
 						// Find if the abr_rate is +ve or -ve
 						let mut payment_amount = abr_value * abr_last_price * position.size;
 						if payment_amount < FixedI128::zero() {
-							payment_amount.neg();
+							payment_amount = payment_amount.saturating_abs();
 						};
 						payment_amount =
 							payment_amount.round_to_precision(collateral_token_decimal.into());
