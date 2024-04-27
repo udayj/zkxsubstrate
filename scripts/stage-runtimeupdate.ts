11456642cb569290d561a911836c939c27f49260
Iurii Karakosov <iurii@zkx.fi>
2024-04-27 09:18:01 +0200

    one more fix  with stage

--- scripts/stage-runtimeupdate.ts
+++ scripts/stage-runtimeupdate.ts
@@ -1,27 +1,23 @@
 // Import the API & Provider and some utility functions
 const { ApiPromise, WsProvider } = require("@polkadot/api");
-
-// import the test keyring (already has dev keys for Alice, Bob, Charlie, Eve & Ferdie)
-const testKeyring = require("@polkadot/keyring/testing");
+const { Keyring } = require("@polkadot/keyring");
 
 const fs = require("fs");
+
+const seedphrase_hex = "0x6a229c16a85e75c30830ed73049574469ab93831aa6b7f118a2b932d958910ef";
 
 async function main() {
   // Initialise the provider to connect to the local node
   const provider = new WsProvider("wss://l3.stage.zkx.fi");
 
+  const keyring = new Keyring({ type: 'sr25519' });
+
   // Create the API and wait until ready (optional provider passed through)
   const api = await ApiPromise.create({ provider });
-
-  // Retrieve the upgrade key from the chain state
-  const adminId = await api.query.sudo.key();
-
-  // Find the actual keypair in the keyring (if this is a changed value, the key
-  // needs to be added to the keyring before - this assumes we have defaults, i.e.
-  // Alice as the key - and this already exists on the test keyring)
-  const keyring = testKeyring.createTestKeyring();
-  const adminPair = keyring.getPair(adminId.toString());
   
+  // Create a key pair from the given public and private keys
+  const adminPair = keyring.addFromUri(seedphrase_hex);
+
   // Retrieve the runtime to upgrade
   const code = fs
     .readFileSync("./node_template_runtime.compact.compressed.wasm")
@@ -31,7 +27,7 @@ async function main() {
       ? api.tx.system.setCode(`0x${code}`) // For newer versions of Substrate
       : api.tx.consensus.setCode(`0x${code}`); // For previous versions
 
-  console.log(`Upgrading from ${adminId}, ${code.length / 2} bytes`);
+  console.log(`Upgrading from ${adminPair}, ${code.length / 2} bytes`);
 
   // Perform the actual chain upgrade via the sudo module
   const sudo = api.tx.sudo.sudoUncheckedWeight(proposal, { weight: 0 });
